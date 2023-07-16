7dc920268f3e03ec24f65c5093cc1e568425dd14
David Lee <leetdavidu@gmail.com>
2023-07-17 01:43:39 +0800

    added sidebar

---
+++ app/src/components/layout/Sidebar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+  import Icon from '@iconify/svelte';
+
+  let currentTile: number = 0;
+</script>
+
+<AppRail>
+  <svelte:fragment slot="lead">
+    <AppRailAnchor href="/">
+      <span>
+        <Icon icon="mdi:home" height={32} class="mx-auto" />
+        Home
+      </span>
+    </AppRailAnchor>
+  </svelte:fragment>
+
+  <!-- --- -->
+  <svelte:fragment slot="trail">
+    <AppRailAnchor
+      href="https://github.com/leetdavid/starrail-manager"
+      target="_blank"
+      title="GitHub"
+    >
+      <Icon icon="mdi:github" height={32} class="mx-auto" />
+      GitHub
+    </AppRailAnchor>
+  </svelte:fragment>
+</AppRail>
