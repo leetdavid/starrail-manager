b536031f5826b3c45aad5ada35141e8bdfffa253
David Lee <leetdavidu@gmail.com>
2023-07-17 01:53:24 +0800

    prettier layouting

--- app/src/components/HSRNote.svelte
+++ app/src/components/HSRNote.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { IHSRNote } from 'hoyoapi';
 
-  import { Avatar, ProgressBar } from '@skeletonlabs/skeleton';
+  import { Avatar, ProgressBar, filter, XPro } from '@skeletonlabs/skeleton';
 
   export let note: IHSRNote;
 
@@ -30,21 +30,77 @@
 
   <hr class="my-3" />
 
-  <h4 class="h4">Expeditions</h4>
+  <div class="flex">
+    <h4 class="h4 flex-1">Expeditions</h4>
+    <span class="flex-0">{note.accepted_epedition_num} accepted / {note.total_expedition_num} total</span>
+  </div>
   <div class="py-4 grid grid-cols-2 grid-rows-2 gap-4">
     {#each note.expeditions as expedition}
       <div class="card card-hover p-4 shadow">
         <h5 class="h5">{expedition.name}</h5>
         <div class="flex gap-2">
           {#each expedition.avatars as avatar}
-            <Avatar width="w-14" src={avatar} border="border-2 border-secondary-700 hover:!border-secondary-500" size="sm" />
+            <Avatar
+              width="w-14"
+              src={avatar}
+              action={filter} actionParams="#Apollo"
+              border="border-2 border-secondary-700 hover:!border-secondary-500"
+            />
           {/each}
         </div>
-        <p class="text-sm">{expedition.status}</p>
+        <p class="text-sm">{expedition.status}, {convert(expedition.remaining_time)} left</p>
       </div>
     {/each}
   </div>
 </div>
+
+<!-- 
+  {
+    "current_stamina": 18,
+    "max_stamina": 180,
+    "stamina_recover_time": 58178,
+    "accepted_epedition_num": 4,
+    "total_expedition_num": 4,
+    "expeditions": [
+        {
+            "avatars": [
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/622524fb899d4f73c7c9651a459c4b3f.png",
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/3f042834dd487f2561f93ee0fa479cc0.png"
+            ],
+            "status": "Ongoing",
+            "remaining_time": 70603,
+            "name": "Winter Soldiers"
+        },
+        {
+            "avatars": [
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/e8b133bacc72e35c04bda62470834edd.png",
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/989cc9be79c8a07a926ad11d767a1b52.png"
+            ],
+            "status": "Ongoing",
+            "remaining_time": 70658,
+            "name": "Fire Lord Inflames Blades of War"
+        },
+        {
+            "avatars": [
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/8634df91ca72ed023b77261c12a0e5cc.png",
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/12dfb0593f88f9bb903121b597d5a762.png"
+            ],
+            "status": "Ongoing",
+            "remaining_time": 70607,
+            "name": "Nameless Land, Nameless People"
+        },
+        {
+            "avatars": [
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/94eafa6314de2c87ccb13c957938f478.png",
+                "https://act.hoyoverse.com/darkmatter/hkrpg/prod_gf_cn/item_icon_76512l/906dae830cf8bc7057b50c73074869ec.png"
+            ],
+            "status": "Ongoing",
+            "remaining_time": 70652,
+            "name": "The Invisible Hand"
+        }
+    ]
+}
+ -->
 
 <!-- 
 {
