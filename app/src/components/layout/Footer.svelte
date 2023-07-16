ef9fa30345bbae10b4def61a883ec5d8f0a766ec
David Lee <leetdavidu@gmail.com>
2023-07-17 02:31:15 +0800

    centered text

--- app/src/components/layout/Footer.svelte
+++ app/src/components/layout/Footer.svelte
@@ -5,7 +5,7 @@
 <div class="page-footer bg-surface-50 dark:bg-surface-900 border-t border-surface-500/10 text-xs md:text-base">
   <div class="w-full max-w-7xl mx-auto p-4 py-4 md:py-8 space-y-8">
     <section class="flex flex-col md:flex-row justify-between items-center md:items-start space-y-4 md:space-y-0">
-      <p>
+      <p class="my-auto">
         <a class="anchor" href="https://github.com/leetdavid/starrail-manager/blob/main/LICENSE" target="_blank" rel="noreferrer">
           MIT License
         </a>
